9f3c214c41853ba40e806fd638eba78984894eec
Samuel Bancroft <samuelbancroft@hotmail.com>
2019-01-06 02:14:41 +0000

    Add project files.

---
+++ Whoops EF Database Example/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whoops_EF_Database_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
